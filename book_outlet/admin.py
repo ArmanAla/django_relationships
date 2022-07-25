8c3ae1f1d7492eecceace092096226fc7fb5cc30
Ala9921160003 <90964584+Ala9921160003@users.noreply.github.com>
2022-07-25 17:08:57 +0430

    AuthorAdmin class was added to admin.py .

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author
 
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author", "rating")
     list_display = ("title", "author")
+    
+    
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
